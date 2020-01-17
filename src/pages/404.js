ba29c7994a1d88643d25bcad4335bbb81f2bc9d3
Reece Mercer <reecemercer981@gmail.com>
2020-01-17 13:45:21 +0000

    broken

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,5 @@ import Layout from '../components/layout'
 import { Link } from 'gatsby'
 
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>404 :(</h1>
-    <p>There's nothing here just yet</p>
-    <Link to="/" className="button">Go back</Link>
-  </Layout>
-)
 
 export default NotFoundPage
